44c3685e2e8ca5ee4e2211c68b20eeefc9ee4120
Frank Macreery <frank@macreery.com>
2013-12-06 15:07:12 -0500

    Make :ci the default task

--- Rakefile
+++ Rakefile
@@ -2,5 +2,3 @@ require 'bundler/gem_tasks'
 
 require 'aptible/tasks'
 Aptible::Tasks.load_tasks
-
-task default: :ci
---
+++ spec/tasks/default_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'rake default' do
+  include_context 'rake'
+
+  its(:prerequisites) { should == ['ci'] }
+end
